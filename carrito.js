3ae79e6cfc67a864f24ab42e938dc934fc19a40c
VivaYo Local <nastychandall@hotmail.com>
2026-06-02 17:19:44 +0100

    chore: Creación ficheros componentes

---
+++ carrito.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function carrito() {
+  return (
+    <div>
+      <h1>carrito</h1>
+    </div>
+  );
+}
+
+export default carrito;
---
+++ producto.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function producto() {
+  return (
+    <div>
+      <h1>producto</h1>
+    </div>
+  );
+}
+
+export default producto;
